ba9b520269200a61cf186d271f840156d59904a7
Marco Túlio de Ávila Santos <marcotulio.avila@gmail.com>
2016-02-26 01:59:43 -0300

    [modify] atualizado arquivo index.blade.php
    Foram alterados os imports dos arquivos .js e .css da aplicação e dos
    componentes de terceiros.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,14 +6,10 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Material Admin</title>
+        <title>{{ $title }}</title>
 
         <!-- Vendor CSS -->
-        <link href="front-end/vendors/bower_components/animate.css/animate.min.css" rel="stylesheet">
-        <link href="front-end/vendors/bower_components/material-design-iconic-font/dist/css/material-design-iconic-font.min.css" rel="stylesheet">
-        <link href="front-end/vendors/bower_components/bootstrap-sweetalert/lib/sweet-alert.css" rel="stylesheet">
-        <link href="front-end/vendors/bower_components/angular-loading-bar/src/loading-bar.css" rel="stylesheet">
-        <link href="front-end/vendors/bower_components/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.min.css" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="front-end/css/vendor.min.css">
 
         <!-- CSS -->
         <link href="front-end/css/app.min.1.css" rel="stylesheet" id="app-level">
@@ -22,9 +18,9 @@
 
     </head>
 
-    <body ng-class="{ 'sw-toggled': mactrl.layoutType === '1'}">
+    <body ng-class="{ 'sw-toggled': true}">
 
-        <data ui-view></data>
+        <div ui-view="index"></div>
 
         <!-- Older IE warning message -->
         <!--[if lt IE 9]>
@@ -71,64 +67,14 @@
 
 
         <!-- Core -->
-        <script src="front-end/vendors/bower_components/jquery/dist/jquery.min.js"></script>
-
-        <!-- Angular -->
-        <script src="front-end/vendors/bower_components/angular/angular.min.js"></script>
-        <script src="front-end/vendors/bower_components/angular-animate/angular-animate.min.js"></script>
-        <script src="front-end/vendors/bower_components/angular-resource/angular-resource.min.js"></script>
-
-        <!-- Angular Modules -->
-        <script src="front-end/vendors/bower_components/angular-ui-router/release/angular-ui-router.min.js"></script>
-        <script src="front-end/vendors/bower_components/angular-loading-bar/src/loading-bar.js"></script>
-        <script src="front-end/vendors/bower_components/oclazyload/dist/ocLazyLoad.min.js"></script>
-        <script src="front-end/vendors/bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js"></script>
-
-        <!-- Common Vendors -->
-        <script src="front-end/vendors/bower_components/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.concat.min.js"></script>
-        <script src="front-end/vendors/bower_components/bootstrap-sweetalert/lib/sweet-alert.min.js"></script>
-        <script src="front-end/vendors/bower_components/Waves/dist/waves.min.js"></script>
-        <script src="front-end/vendors/bootstrap-growl/bootstrap-growl.min.js"></script>
-        <script src="front-end/vendors/bower_components/ng-table/dist/ng-table.min.js"></script>
-
+        <script src="front-end/js/vendor.min.js"></script>
 
         <!-- Placeholder for IE9 -->
         <!--[if IE 9 ]>
             <script src="front-end/vendors/bower_components/jquery-placeholder/jquery.placeholder.min.js"></script>
         <![endif]-->
 
-        <!-- Using below vendors in order to avoid misloading on resolve -->
-        <script src="front-end/vendors/bower_components/flot/jquery.flot.js"></script>
-        <script src="front-end/vendors/bower_components/flot.curvedlines/curvedLines.js"></script>
-        <script src="front-end/vendors/bower_components/flot/jquery.flot.resize.js"></script>
-        <script src="front-end/vendors/bower_components/moment/min/moment.min.js"></script>
-        <script src="front-end/vendors/bower_components/fullcalendar/dist/fullcalendar.min.js"></script>
-        <script src="front-end/vendors/bower_components/flot-orderBars/js/jquery.flot.orderBars.js"></script>
-        <script src="front-end/vendors/bower_components/flot/jquery.flot.pie.js"></script>
-        <script src="front-end/vendors/bower_components/flot.tooltip/js/jquery.flot.tooltip.min.js"></script>
-        <script src="front-end/vendors/bower_components/angular-nouislider/src/nouislider.min.js"></script>
-
-
         <!-- App level -->
         <script src="front-end/js/app.min.js"></script>
-        <!-- <script src="front-end/js/app.js"></script>
-        <script src="front-end/js/config.js"></script>
-        <script src="front-end/js/controllers/main.js"></script>
-        <script src="front-end/js/services.js"></script>
-        <script src="front-end/js/templates.js"></script>
-        <script src="front-end/js/controllers/ui-bootstrap.js"></script>
-        <script src="front-end/js/controllers/table.js"></script> -->
-
-
-        <!-- Template Modules -->
-        <!-- <script src="front-end/js/modules/template.js"></script>
-        <script src="front-end/js/modules/ui.js"></script>
-        <script src="front-end/js/modules/charts/flot.js"></script>
-        <script src="front-end/js/modules/charts/other-charts.js"></script>
-        <script src="front-end/js/modules/form.js"></script>
-        <script src="front-end/js/modules/media.js"></script>
-        <script src="front-end/js/modules/components.js"></script>
-        <script src="front-end/js/modules/calendar.js"></script>
-        <script src="front-end/js/modules/demo.js"></script> -->
     </body>
 </html>
